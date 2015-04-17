6d62777d24760c47cff44d1f0053b08b4a766de5
Lauren Molley <lmolley@umich.edu>
2015-04-16 20:40:09 -0400

    small changes to tutorial

--- src/tutorial.cpp
+++ src/tutorial.cpp
@@ -26,8 +26,8 @@ Tutorial::Tutorial()
     layout->addWidget(next);
 
     std::vector<Flashcard*> mocFlashcards = std::vector<Flashcard*>();
-    mocFlashcards.push_back(new Flashcard("You can flip the flashcard by clicking anywhere on the flashcard."
-                                          , "If you got the correct answer, click the green check. If you got the wrong answer, click the red x.", 0));
+    mocFlashcards.push_back(new Flashcard("Click anywhere on the flashcard to flip it. Try doing it now."
+                                          , "If you got the correct answer, click the green check. If you got the wrong answer, click the red x. Click either now.", 0));
 
     mocFlashcards.push_back(new Flashcard("This will update your card score and take you to the next card. Flip me for more tips."
                                           , "Instead of clicking the green check, you can also press the left keyboard key. Likewise, you can press the right keyboard key instead of clicking the red x. Try it now.", 0));
@@ -84,20 +84,22 @@ QFrame* Tutorial::createUserTutPage()
     QFrame* frame = new QFrame(this);
     frame->setMaximumSize(700,500);
     QLabel* line1 = new QLabel("You've just successfully created a new user!");
-    QLabel* line2 = new QLabel("If you are new to FlashcardsPro, "
-                               "we suggest that you continue through this quick tutorial.");
-    QLabel* line3 = new QLabel("You can exit the tutorial at any time by closing this window.");
-    QLabel* line4 = new QLabel("Click the next button to proceed...");
+    QLabel* line2 = new QLabel("If you are new to FlashcardsPro, ");
+    QLabel* line3 = new QLabel("we suggest that you continue through this quick tutorial.");
+    QLabel* line4 = new QLabel("You can exit the tutorial at any time by closing this window.");
+    QLabel* line5 = new QLabel("Click the next button to proceed...");
     line1->setFont(naxa);
     line2->setFont(naxa);
     line3->setFont(naxa);
-    line4->setFont(naxa_b);
+    line4->setFont(naxa);
+    line5->setFont(naxa_b);
 
     QVBoxLayout* layout = new QVBoxLayout();
     layout->addWidget(line1, 0, Qt::AlignHCenter);
     layout->addWidget(line2, 0, Qt::AlignHCenter);
     layout->addWidget(line3, 0, Qt::AlignHCenter);
     layout->addWidget(line4, 0, Qt::AlignHCenter);
+    layout->addWidget(line5, 0, Qt::AlignHCenter);
     frame->setLayout(layout);
     return frame;
 }
@@ -113,21 +115,27 @@ QFrame* Tutorial::createDecksViewTutPage()
 
     QFrame* frame = new QFrame(this);
     frame->setMaximumSize(700,500);
-    QLabel* line1 = new QLabel("Selecting your username will bring you to all of your decks. There are currently no decks created.");
-    QLabel* line2 = new QLabel("Clicking on the plus sign will let you load a .dek file or create a new deck with our deck builder.");
-    QLabel* line3 = new QLabel("Try clicking the plus sign and then click next to proceed...");
+    QLabel* line1 = new QLabel("Selecting your username will bring you to all of your decks. ");
+    QLabel* line2 = new QLabel("There are currently no decks created.");
+    QLabel* line3 = new QLabel("Clicking on the plus sign will let you load a .dek file ");
+    QLabel* line4 = new QLabel("or create a new deck with our deck builder.");
+    QLabel* line5 = new QLabel("Try clicking the plus sign and then click next to proceed...");
     line1->setFont(naxa);
     line2->setFont(naxa);
     line3->setFont(naxa);
+    line4->setFont(naxa);
+    line5->setFont(naxa_b);
 
     DecksView* mocDecksView = new DecksView(NULL, NULL);
     mocDecksView->setStyleSheet("background-color: rgb(191, 197, 255);");
 
     QVBoxLayout* layout = new QVBoxLayout();
     layout->addWidget(line1, 0, Qt::AlignHCenter);
-    layout->addWidget(mocDecksView, 0, Qt::AlignHCenter);
     layout->addWidget(line2, 0, Qt::AlignHCenter);
+    layout->addWidget(mocDecksView, 0, Qt::AlignHCenter);
     layout->addWidget(line3, 0, Qt::AlignHCenter);
+    layout->addWidget(line4, 0, Qt::AlignHCenter);
+    layout->addWidget(line5, 0, Qt::AlignHCenter);
     frame->setLayout(layout);
     return frame;
 }
@@ -169,12 +177,15 @@ QFrame* Tutorial::createStudyAreaTutPage()
 
     QFrame* frame = new QFrame(this);
     frame->setMaximumSize(700,500);
-    QLabel* line1 = new QLabel("Clicking study deck brings you to the Study Area. Click on the flashcards to get useful tips.");
+    QLabel* line1 = new QLabel("Clicking study deck brings you to the Study Area. ");
+    QLabel* line2 = new QLabel( "Click on the flashcards to get useful tips.");
     line1->setFont(naxa);
+    line2->setFont(naxa);
 
     StudyArea* mocStudyArea = new StudyArea(mocDeck, NULL, mocMenu);
     QVBoxLayout* layout = new QVBoxLayout();
     layout->addWidget(line1,0, Qt::AlignHCenter);
+    layout->addWidget(line2,0, Qt::AlignHCenter);
     layout->addWidget(mocStudyArea,0, Qt::AlignHCenter);
     frame->setLayout(layout);
     return frame;
